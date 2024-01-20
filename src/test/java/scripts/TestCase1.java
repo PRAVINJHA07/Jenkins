1616bd2453317429b9e5960577d117b50d7b3912
praveen <praveen@DGL061-DM-DINESH>
2024-01-21 01:57:01 +0530

    Jenkins Project

---
+++ src/test/java/scripts/TestCase1.java
@@ -0,0 +1,15 @@
+package scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TestCase1 {
+	
+	@Test
+	public void tc1() {
+		
+		Reporter.log("Hello World", true);
+		
+	}
+
+}
